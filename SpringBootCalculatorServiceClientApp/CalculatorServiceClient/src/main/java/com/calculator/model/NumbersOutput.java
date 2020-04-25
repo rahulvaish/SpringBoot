2f486fd8919fa0acd5125bdc1942fd9c70f8203f
Rahul <vaish.rahul4810@gmail.com>
2020-04-25 14:51:31 +0530

    SBCalc

---
+++ SpringBootCalculatorServiceClientApp/CalculatorServiceClient/src/main/java/com/calculator/model/NumbersOutput.java
@@ -0,0 +1,33 @@
+package com.calculator.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NumbersOutput {
+	private int intResult;
+	private float floatResult;
+
+	public NumbersOutput() {
+		super();
+	}
+
+	public int getIntResult() {
+		return intResult;
+	}
+
+	public void setIntResult(int intResult) {
+		this.intResult = intResult;
+	}
+
+	public float getFloatResult() {
+		return floatResult;
+	}
+
+	public void setFloatResult(float floatResult) {
+		this.floatResult = floatResult;
+	}
+
+	
+	
+	
+}
